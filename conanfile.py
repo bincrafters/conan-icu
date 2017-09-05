27ebd442104406d67810ab66c7a09819727e508c
Jerry W <jerrywiltse@gmail.com>
2017-09-04 21:27:28 -0400

    tried fix recipe

--- conanfile.py
+++ conanfile.py
@@ -8,20 +8,26 @@ class IcuConan(ConanFile):
     description = 'ICU is a mature, widely used set of C/C++ and Java libraries providing Unicode and Globalization support for software applications.'
     url = "https://github.com/bincrafters/conan-icu"
     settings = "os", "arch", "compiler", "build_type"
-    source_url = f"http://download.icu-project.org/files/icu4c/{version}/icu4c-{version.replace('.', '_')}-src"
+    source_url = "http://download.icu-project.org/files/icu4c/{0}/icu4c-{1}-src".format(version,version.replace('.', '_'))
 
     def source(self):
-        tools.get(f'{self.source_url}.zip')
+        tools.get("{0}.zip".format(self.source_url))
 
     def build(self):
         if self.settings.os == 'Windows':
             sln_file = os.path.join(self.name, "source","allinone","allinone.sln")
             vcvars_command = tools.vcvars_command(self.settings)
             build_command = tools.build_sln_command(self.settings, sln_file)
-            command = "%s && %s" % (vcvars_command, build_command)
+            command = "{0} && {1}".format(vcvars_command, build_command)
             self.run(command)
         else:
-            self.run(f'bash {root_path}/build_icu_{self.settings.build_type}.sh {root_path} {platform} {root_path}/install_{self.settings.build_type}')
+            root_path = self.conanfile_directory.replace('\\', '/')
+            platform = ''
+            if self.settings.os == 'Linux':
+                platform = 'Linux'
+            elif self.settings.os == 'MacOs':
+                platform = 'MacOSX'
+            self.run("bash {0}/build_icu_{1}.sh {0} {2} {0}/install_{3}").format(root_path,self.settings.build_type,platform,self.settings.build_type)
 
     def package(self):
         install_path = f'install_{self.settings.build_type}'
