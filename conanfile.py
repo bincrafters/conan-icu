1897dba8a4f6f868cf663bc3f3e77d0e9751ad09
Rene Rivera <grafikrobot@gmail.com>
2017-09-16 12:53:36 -0500

    Building on OSX, and likely any Unix.
    Change Linux/OSX build to the new ICU configure and build methods. Also
    add debug vs. release building. Narrow down to only package the i18n
    main library.

--- conanfile.py
+++ conanfile.py
@@ -11,47 +11,42 @@ class IcuConan(ConanFile):
     source_url = "http://download.icu-project.org/files/icu4c/{0}/icu4c-{1}-src".format(version,version.replace('.', '_'))
 
     def source(self):
-        tools.get("{0}.zip".format(self.source_url))
+        if self.settings.os == 'Windows':
+            tools.get("{0}.zip".format(self.source_url))
+        else:
+            tools.get("{0}.tgz".format(self.source_url))
 
     def build(self):
+        root_path = self.conanfile_directory.replace('\\', '/')
+        src_path = os.path.join(root_path, self.name, 'source')
         if self.settings.os == 'Windows':
-            sln_file = os.path.join(self.name, "source","allinone","allinone.sln")
+            sln_file = os.path.join(src_path,"allinone","allinone.sln")
             vcvars_command = tools.vcvars_command(self.settings)
             build_command = tools.build_sln_command(self.settings, sln_file, targets=["i18n"])
             command = "{0} && {1}".format(vcvars_command, build_command)
             self.run(command)
         else:
-            root_path = self.conanfile_directory.replace('\\', '/')
             platform = ''
             if self.settings.os == 'Linux':
-                platform = 'Linux'
-            elif self.settings.os == 'MacOs':
+                if self.settings.compiler == 'gcc':
+                    platform = 'Linux/gcc'
+                else:
+                    platform = 'Linux'
+            elif self.settings.os == 'Macos':
                 platform = 'MacOSX'
-            self.run("bash {0}/build_icu_{1}.sh {0} {2} {0}/install_{3}").format(root_path,self.settings.build_type,platform,self.settings.build_type)
+            enable_debug = ''
+            if self.settings.build_type == 'Debug':
+                enable_debug = '--enable-debug'
+            self.run("cd {0} && bash runConfigureICU {1} {2} --prefix={3}".format(
+                src_path, enable_debug, platform, os.path.join(root_path,'output')))
+            self.run("cd {0} && make install".format(src_path))
 
     def package(self):
-        install_path = "install_{0}".format(self.settings.build_type)
+        install_path = "output"
         self.copy("*", "include", "{0}/include".format(install_path), keep_path=True)
-
-        self.copy("*.exe", "bin", "{0}/bin".format(install_path), keep_path=False)
-        self.copy("*.lib", "lib", "{0}/lib".format(install_path), keep_path=False)
-        self.copy("*.pdb", "lib", "build_{0}/lib".format(self.settings.build_type), keep_path=False)
-        self.copy("*.pdb", "lib", "build_{0}/tools/toolutil".format(self.settings.build_type), keep_path=False)
-        self.copy("*.dll", "bin", "{0}/bin".format(install_path), keep_path=False)
-        self.copy("*.pdb", "bin", "{0}/lib".format(install_path), keep_path=False)
-
-        self.copy("*.dat", "data", "{0}/share".format(install_path), keep_path=False)
-
-        if self.settings.build_type == 'Debug':
-            self.copy("*.cpp", "src", "icu", keep_path=True)
-            self.copy("*.hpp", "src", "icu", keep_path=True)
-            self.copy("*.h", "src", "icu", keep_path=True)
-            self.copy("*.c", "src", "icu", keep_path=True)
+        self.copy("*icui18n.dll", "lib", "{0}/lib".format(install_path), keep_path=False)
+        self.copy("*icui18n.dylib", "lib", "{0}/lib".format(install_path), keep_path=False)
+        self.copy("*icui18n.so", "lib", "{0}/lib".format(install_path), keep_path=False)
 
     def package_info(self):
-        self.cpp_info.defines = ['U_STATIC_IMPLEMENTATION=1', 'U_CHARSET_IS_UTF8=1', 'UNISTR_FROM_CHAR_EXPLICIT=explicit', 'UNISTR_FROM_STRING_EXPLICIT=explicit', 'U_NO_DEFAULT_INCLUDE_UTF_HEADERS=1']
-        if self.settings.compiler == 'Visual Studio':
-            if self.settings.build_type == 'Debug':
-                self.cpp_info.libs = ["sicuucd", "sicuind", "sicudtd", 'Advapi32']
-            elif self.settings.build_type == 'Release':
-                self.cpp_info.libs = ["sicuuc", "sicuin", "sicudt", 'Advapi32']
+        self.cpp_info.libs = self.collect_libs()
