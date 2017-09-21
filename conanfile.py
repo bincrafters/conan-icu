8a4994a1991497dace62417d22cb8964cc38fd46
Rene Rivera <grafikrobot@gmail.com>
2017-09-21 13:45:46 -0500

    Finally builds correct data DLL on Windows.

--- conanfile.py
+++ conanfile.py
@@ -1,16 +1,14 @@
 from conans import ConanFile, tools
 import os
 
-
 class IcuConan(ConanFile):
     name = "icu"
     version = "59.1"
-    license = "http://www.unicode.org/copyright.html#License"
+    license="http://www.unicode.org/copyright.html#License"
     description = 'ICU is a mature, widely used set of C/C++ and Java libraries providing Unicode and Globalization support for software applications.'
     url = "https://github.com/bincrafters/conan-icu"
     settings = "os", "arch", "compiler", "build_type"
-    source_url = "http://download.icu-project.org/files/icu4c/{0}/icu4c-{1}-src".format(version,
-                                                                                        version.replace('.', '_'))
+    source_url = "http://download.icu-project.org/files/icu4c/{0}/icu4c-{1}-src".format(version,version.replace('.', '_'))
     options = {"with_io": [True, False]}
     default_options = "with_io=False"
 
@@ -21,36 +19,33 @@ class IcuConan(ConanFile):
             tools.get("{0}.tgz".format(self.source_url))
 
     def build(self):
-        src_path = os.path.join(self.name, 'source')
-
-        makedata_file = os.path.join(src_path, 'data', 'makedata.mak')
-        tools.replace_in_file(makedata_file,
-                              r'GODATA "$(ICU_LIB_TARGET)" "$(TESTDATAOUT)\testdata.dat"',
-                              'GODATA "$(ICU_LIB_TARGET)"')
-
+        root_path = self.conanfile_directory
+        tools.replace_in_file(
+            os.path.join(root_path, self.name,'source','data','makedata.mak'),
+            r'GODATA "$(ICU_LIB_TARGET)" "$(TESTDATAOUT)\testdata.dat"',
+            r'GODATA "$(ICU_LIB_TARGET)"')
+        src_path = os.path.join(root_path, self.name, 'source')
         if self.settings.os == 'Windows':
-            sln_file = os.path.join(src_path, "allinone", "allinone.sln")
+            sln_file = os.path.join(src_path,"allinone","allinone.sln")
             vcvars_command = tools.vcvars_command(self.settings)
-            targets = ["i18n", "common", "pkgdata"]
+            targets = ["i18n","common","pkgdata"]
             if self.options.with_io:
                 targets.append('io')
             build_command = tools.build_sln_command(self.settings, sln_file, targets=targets)
-            build_command = build_command.replace('"x86"', '"Win32"')
+            build_command = build_command.replace('"x86"','"Win32"')
             command = "{0} && {1}".format(vcvars_command, build_command)
             self.run(command)
             cfg = 'x64' if self.settings.arch == 'x86_64' else 'x86'
-            cfg += "\\" + str(self.settings.build_type)
-            data_dir = src_path + "\\data"
-            bin_dir = data_dir + "\\..\\..\\bin"
+            cfg += "\\"+str(self.settings.build_type)
+            data_dir = src_path+"\\data"
+            bin_dir = data_dir+"\\..\\..\\bin"
             if self.settings.arch == 'x86_64':
                 bin_dir += '64'
-            icudt_target = bin_dir + "\\icudt{0}.dll".format(self.version.split('.')[0])
+            icudt_target = bin_dir+"\\icudt{0}.dll".format(self.version.split('.')[0])
             makedata = '{vcvars} && cd {datadir} && nmake /a /f makedata.mak ICUMAKE="{datadir}" CFG={cfg}'.format(
                 vcvars=vcvars_command,
                 datadir=data_dir,
-                cfg=cfg,
-                target=icudt_target)
-            # os.remove(icudt_target)
+                cfg=cfg)
             self.output.info(makedata)
             self.run(makedata)
         else:
@@ -66,12 +61,12 @@ class IcuConan(ConanFile):
             if self.settings.build_type == 'Debug':
                 enable_debug = '--enable-debug'
             self.run("cd {0} && bash runConfigureICU {1} {2} --prefix={3}".format(
-                src_path, enable_debug, platform, 'output'))
+                src_path, enable_debug, platform, os.path.join(root_path,'output')))
             self.run("cd {0} && make install".format(src_path))
 
     def package(self):
         if self.settings.os == 'Windows':
-            self.copy("*", "include", os.path.join(self.name, "include"), keep_path=True)
+            self.copy("*", "include", os.path.join(self.name,"include"), keep_path=True)
             libs = ['in', 'uc', 'dt']
             if self.options.with_io:
                 libs.append('io')
@@ -95,10 +90,8 @@ class IcuConan(ConanFile):
             if self.options.with_io:
                 libs.append('io')
             for lib in libs:
-                self.copy(pattern="*icu{0}.dylib".format(lib), dst="lib", src="{0}/lib".format(install_path),
-                          keep_path=False)
-                self.copy(pattern="*icu{0}.so".format(lib), dst="lib", src="{0}/lib".format(install_path),
-                          keep_path=False)
+                self.copy(pattern="*icu{0}.dylib".format(lib), dst="lib", src="{0}/lib".format(install_path), keep_path=False)
+                self.copy(pattern="*icu{0}.so".format(lib), dst="lib", src="{0}/lib".format(install_path), keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = self.collect_libs()
