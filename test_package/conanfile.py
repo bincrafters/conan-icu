c9096b998b5e5b445fb433060f386479324669f3
Jerry W <jerrywiltse@gmail.com>
2017-09-24 03:00:53 -0400

    try chdir test_package [skip appveyor]

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -16,6 +16,5 @@ class TestPackageConan(ConanFile):
         self.copy("*", dst="bin", src="lib")
         
     def test(self):
-        self.run("ls -l")
-        self.run('find . -name "*"')
-        self.run(os.path.join("bin","test_package"))
+        with tools.chdir(os.path.join(os.getcwd(),"bin")):
+            self.run(os.path.join(os.getcwd(),"test_package"))
