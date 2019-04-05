b9b0397c10e11a6a2947e4301da7982ef920032b
SSE4 <tomskside@gmail.com>
2019-04-05 22:36:01 +0700

    - update from templates
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- .ci/install.sh
+++ .ci/install.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-set -e
-set -x
+set -ex
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
     brew update || brew update
@@ -13,13 +12,13 @@ if [[ "$(uname -s)" == 'Darwin' ]]; then
         eval "$(pyenv init -)"
     fi
 
-    pyenv install 2.7.14
-    pyenv virtualenv 2.7.14 conan
+    pyenv install 3.7.1
+    pyenv virtualenv 3.7.1 conan
     pyenv rehash
     pyenv activate conan
 fi
 
 pip install conan --upgrade
-pip install conan_package_tools
+pip install conan_package_tools bincrafters_package_tools
 
 conan user
