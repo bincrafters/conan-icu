a054a78e8713a416902f7f89597cd4a3fd0883ed
SSE4 <tomskside@gmail.com>
2019-04-12 16:51:24 +0700

    - try to fix installer build (set arch_build)
    Signed-off-by: SSE4 <tomskside@gmail.com>

--- build.py
+++ build.py
@@ -2,10 +2,16 @@
 # -*- coding: utf-8 -*-
 
 
-from bincrafters import build_template_default
+import os
+from bincrafters import build_template_default, build_template_installer
 
 if __name__ == "__main__":
 
-    builder = build_template_default.get_builder(pure_c=False)
+    if "ARCH" in os.environ:
+        arch = os.environ["ARCH"]
+        builder = build_template_installer.get_builder()
+        builder.add({"os" : build_shared.get_os(), "arch_build" : arch, "arch": arch}, {}, {}, {})
+    else:
+        builder = build_template_default.get_builder(pure_c=False)
 
     builder.run()
