c3021f2d95dbf843e48ff3552459f748be554a07
Julien Marrec <julien.marrec@gmail.com>
2019-05-21 14:44:24 +0200

    Lint build.py

--- build.py
+++ build.py
@@ -3,14 +3,19 @@
 
 
 import os
-from bincrafters import build_template_default, build_template_installer, build_shared
+from bincrafters import (build_template_default,
+                         build_template_installer,
+                         build_shared)
 
 if __name__ == "__main__":
 
     if "ARCH" in os.environ:
         arch = os.environ["ARCH"]
         builder = build_template_installer.get_builder()
-        builder.add({"os" : build_shared.get_os(), "arch_build" : arch, "arch": arch}, {}, {}, {})
+        builder.add({"os": build_shared.get_os(),
+                     "arch_build": arch,
+                     "arch": arch},
+                    {}, {}, {})
     else:
         builder = build_template_default.get_builder(pure_c=False)
 
