6e8886e9c7aca2e359b1c4961d39a88e73242070
Kevin A. Mitchell <kam@datalogics.com>
2019-10-02 10:27:14 -0500

    Fix build on Windows; support LLVM toolkit (#3)
    * fix(conan): install dll files in bin directory when build with msvc
    
    * Build with a more recent msys2
    
    - Resolves bug with the shell crashing during configure.
    
    - Also, don’t apply the Python path patches; they seem to break the
      build.
    
    * Enable build with LLVM on Windows
    
    - If building on Windows, and LLVM is in the name of the toolset,
      edit the runConfigureICU file to change the compiler from 'cl' to
      'clang-cl'.
    
    * Cross compiling: use the datalogics/stable channel

--- icu_base.py
+++ icu_base.py
@@ -39,7 +39,9 @@ class ICUBase(ConanFile):
     def build_requirements(self):
         if self._the_os == "Windows":
             #self.build_requires("cygwin_installer/2.9.0@bincrafters/stable")
-            self.build_requires("msys2_installer/20161025@bincrafters/stable")
+            self.build_requires("msys2_installer/latest@bincrafters/stable")
+            if self.settings.compiler == "gcc" and tools.os_info.is_windows:
+                self.build_requires("mingw_installer/1.0@conan/stable")
 
     def source(self):
         version = self.version.replace('.', '-')
@@ -84,7 +86,12 @@ class ICUBase(ConanFile):
             tools.replace_in_file(run_configure_icu_file, "-MDd", flags)
             tools.replace_in_file(run_configure_icu_file, "-MD", flags)
 
-        self._replace_pythonpath() # ICU 64.1
+            toolset = self.settings.get_safe("compiler.toolset") or ""
+            if "LLVM" in toolset.upper():
+                tools.replace_in_file(run_configure_icu_file, "CC=cl", "CC=clang-cl")
+                tools.replace_in_file(run_configure_icu_file, "CXX=cl", "CXX=clang-cl")
+
+        # self._replace_pythonpath() # ICU 64.1
         self._workaround_icu_20545()
 
         self._env_build = AutoToolsBuildEnvironment(self)
